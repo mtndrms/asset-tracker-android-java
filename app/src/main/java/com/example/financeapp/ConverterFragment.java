a8aab31ac2c8ac91533adc8bd6a9a07704e19155
Metin Durmus <mtn.drms@outlook.com>
2022-04-29 13:05:34 +0300

    converter design

--- app/src/main/java/com/example/financeapp/ConverterFragment.java
+++ app/src/main/java/com/example/financeapp/ConverterFragment.java
@@ -2,16 +2,15 @@ package com.example.financeapp;
 
 import static com.example.financeapp.Utils.*;
 
-import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
 import android.widget.Spinner;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -29,23 +28,31 @@ public class ConverterFragment extends Fragment implements AdapterView.OnItemSel
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+        LinearLayout changeSides = view.findViewById(R.id.llChangeSides);
         Spinner spinnerFrom = (Spinner) view.findViewById(R.id.spFrom);
         Spinner spinnerTo = (Spinner) view.findViewById(R.id.spTo);
+
         spinnerFrom.setOnItemSelectedListener(this);
         spinnerTo.setOnItemSelectedListener(this);
 
-        //Creating the ArrayAdapter instance having the country list
         ArrayAdapter aa = new ArrayAdapter(getContext(),android.R.layout.simple_spinner_item, currenciesList());
-        aa.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        aa.setDropDownViewResource(R.layout.dropdown_item);
         spinnerFrom.setAdapter(aa);
         spinnerTo.setAdapter(aa);
+
+        changeSides.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                System.out.println("TEST");
+            }
+        });
     }
 
     @Override
     public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
-        System.out.println(view.getParent());
-        ((TextView) view).setTextColor(Color.RED);
-        Toast.makeText(getContext(), currenciesList().get(i), Toast.LENGTH_LONG).show();
+        if ((TextView) view != null) {
+            ((TextView) view).setTextColor(getResources().getColor(R.color.spinner_selected_color));
+        }
     }
 
     @Override
